56da9e7f45341845ecec1e2a75e29765c1e46c6a
Sanket Dhuri <contact.sanketdhuri@gmail.com>
2023-08-09 08:29:52 +0530

    Add files via upload

--- Nucleus/pages/home/app.js
+++ Nucleus/pages/home/app.js
@@ -1,2 +1,58 @@
+// GSAP
+// var t1 = gsap.timeline();
 
+// t1
+// 	.from(".nav",{
+// 		y:-20,
+// 		duration: 2
+// 	})
+// 	.from(".quote",{
+// 		y:-20,
+// 		x:-10,
+// 		duration: 2,
+// 		opacity: 0,
+// 		delay: -1
+// 	})
+
+
+
+const tl = gsap.timeline();
+
+tl
+	.from(".nav-a", {
+		onStart: function(){
+			$('.nav-a').textillate({
+				in: {effect: 'bounceInDown'}
+			})
+		}
+	})
+	.from(".nav-a-b", {
+		onStart: function(){
+			$('.nav-a-b').textillate({
+				in: {effect: 'bounceInDown'}
+			})
+		}
+	})
+	.from(".quote", {
+		onStart: function(){
+			$('.quote').textillate({
+				in: { effect: 'bounceInUp'}
+			})
+		}
+	})
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".about-text-p", {
+	duration: 3,
+	ScrollTrigger: function(){
+			$('.quote').textillate({
+				in: { effect: 'fadeInUp'}
+			})
+	}
+	markers: true,
+	start: "top 6%",
+	end: "top 1%"
+});
 
